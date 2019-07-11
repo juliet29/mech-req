907e52d4889a527743ca1b17a8ed42b13166db02
Juliet <jnwagwuume-ezeoke@college.harvard.edu>
2019-07-11 15:06:58 +0000

    table done

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // use http.get() to load data from API endpoint
+  list() {
+    return this.http.get('http://127.0.0.1:8000/servicerequest');
+  }
+
 }
