028987bee5c3be02eea3d41c715cc657f9a5f618
Juliet <jnwagwuume-ezeoke@college.harvard.edu>
2019-07-26 11:22:12 +0000

    profile cute

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,43 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from 'src/app/_services/user.service';
-import { RequestService } from 'src/app/_services/request.service';
-
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { UserService } from "src/app/_services/user.service";
+import { RequestService } from "src/app/_services/request.service";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.scss"]
 })
 export class ProfileComponent implements OnInit {
-
-  constructor(private _UserService: UserService, private _RequestService: RequestService ) { }
+  constructor(
+    private _UserService: UserService,
+    private _RequestService: RequestService
+  ) {}
 
   private author_id: number;
-  public u_request: any;
+  public user_request: any;
+  // @ViewChild("toDisplay", { static: true }) toDisplay: ElementRef;
+
+  // ngAfterViewInit() {
+  //   console.log(this.toDisplay);
+  // }
 
   ngOnInit() {
     this.author_id = this._UserService.currentUserValue.id;
-    if ( this.author_id) {
-      this.getUserRequest()
+    if (this.author_id) {
+      this.getUserRequest();
     }
-
   }
 
   getUserRequest() {
-    this._RequestService.list_user(this.author_id).subscribe(
-      data => this.u_request = data,
-      err => console.error(err)
-    )
+    this._RequestService
+      .list_user(this.author_id)
+      .subscribe(data => (this.user_request = data), err => console.error(err));
   }
 
   refreshToken() {
     this._UserService.refreshToken();
   }
 
-  logout(){
+  logout() {
     this._UserService.logout();
   }
 
+  dropdown() {
+    let target = event.target as HTMLElement;
+    let target_id = Number(target.id);
 
+    // the specific dropdown that is being shown
+    let displayElement = document.getElementsByClassName("display-sometimes")[
+      target_id
+    ] as HTMLElement;
 
+    // logi for showing and hiding dropdown
+    if (displayElement.classList.contains("show")) {
+      displayElement.classList.remove("show");
+    } else {
+      displayElement.classList.add("show");
+    }
+  }
 }
